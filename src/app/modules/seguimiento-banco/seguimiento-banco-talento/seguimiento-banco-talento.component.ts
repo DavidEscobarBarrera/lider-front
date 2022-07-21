93d9777f7761cf06fb90a3062c75ee189ff3b356
JhonAcevedo02 <jhonacevedo@seti.com.co>
2022-07-21 11:02:49 -0500

    actualización de detalles de diseño e iniciación de validación de los formularios

--- src/app/modules/seguimiento-banco/seguimiento-banco-talento/seguimiento-banco-talento.component.ts
+++ src/app/modules/seguimiento-banco/seguimiento-banco-talento/seguimiento-banco-talento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-seguimiento-banco-talento',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeguimientoBancoTalentoComponent implements OnInit {
 
+  constructor() { }
   fecha: Date = new Date;
 
-  constructor() { }
+  habilidadesBlandasRec = new FormControl();
+
+  habilidadesTecnicasRec = new FormControl();
+
+  habilidadesBlandasOpor = new FormControl();
+
+  habilidadesTecnicasOpor = new FormControl();
+
+  talentoBancoForm = new FormGroup ({
+    habilidadesTecnicasRec: this.habilidadesTecnicasRec,
+    habilidadesBlandasRec: this.habilidadesBlandasRec,
+    habilidadesTecnicasOpor: this.habilidadesTecnicasOpor,
+    habilidadesBlandasOpor: this.habilidadesBlandasOpor,
+  })
+
+  talentoBanco(){
+    console.log(this.talentoBancoForm.value);
+
+  }
 
   ngOnInit(): void {
   }
