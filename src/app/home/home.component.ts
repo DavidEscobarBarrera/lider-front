85eded423bd627edf25c03c1f90618745a95e629
Julian <craj.0912@gmail.com>
2022-08-16 18:01:53 -0500

    password change done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {SetiLeaderInfoService} from '../services/seti-leader-info.service';
-import {NgbModalConfig, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModalConfig, NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ChangePasswordValidator} from '../validators/change-password-validator';
 
 @Component({
   selector: 'app-home',
@@ -11,17 +12,24 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class HomeComponent implements OnInit, AfterViewInit {
   client: any;
+  hide = true;
+  hide2 = true;
+  hide3 = true;
   @ViewChild('content') private content: any;
+  modalReference!: NgbModalRef;
 
   currentPassword = new FormControl('', [Validators.required]);
-  newPassword = new FormControl('', [Validators.required]);
+  newPassword = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{10,}$/)
+  ]);
   confirmNewPassword = new FormControl('', [Validators.required]);
 
   changePasswordForm = new FormGroup({
     currentPassword: this.currentPassword,
     password: this.newPassword,
     passwordRepeated: this.confirmNewPassword
-  })
+  }, [ChangePasswordValidator.match('password', 'passwordRepeated')])
 
   constructor(
     private leaderInfoService: SetiLeaderInfoService,
@@ -43,7 +51,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
       if (!this.client.cambioContrasena) {
         this.open(this.content)
       }
-    },2000)
+    }, 2000)
   }
 
   open(content: any) {
@@ -51,6 +59,17 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
   submit() {
-    console.log(this.changePasswordForm.value)
+    if (this.changePasswordForm.invalid) {
+      return;
+    }
+    this.leaderInfoService.updatePassword(this.client.pusua_id, this.changePasswordForm.value)
+      .subscribe({
+        next: () => {
+          localStorage.removeItem('user')
+          this.leaderInfoService.getLeaderSetiInfo()
+          this.modalReference.close()
+        },
+        error: err => console.log(err)
+      })
   }
 }
